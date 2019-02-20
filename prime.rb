ed42e415220cc91a5c38fd27e82f954441f90cad
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 05:53:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,18 @@
 def prime?(num)
   n = 5
   num = num.abs
-  if (num != 2 && num != 3) && (num%2 == 0 || num%3 == 0 || num == 1)
+  if num != 2 && num != 3 && (num%2 == 0 || num%3 == 0 || num == 1)
     return false
   elsif (num <= 64)
     while n < num do 
-      if (num%n == 0)
+      if num%n == 0
         return false
       end
     end
     return true
   else
     while n < Math.sqrt(num).floor do
-      if (num%n == 0)
+      if num%n == 0
         return false
       end
     end
