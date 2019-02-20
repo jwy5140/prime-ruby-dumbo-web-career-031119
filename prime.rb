6814bcf13476a88ab4d404151f3d0b1b3dcf5e13
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 05:57:40 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(num)
+  n = 5
+  num = num.abs
+  if num != 2 && num != 3 && (num%2 == 0 || num%3 == 0 || num == 1)
+    return false
+  elsif (num <= 64)
+    while n < num do 
+      if num%n == 0
+        return false
+      end
+      n += 1 
+    end
+    return true
+  else
+    while n < Math.sqrt(num).ceil do
+      if num%n == 0
+        return false
+      end
+      n += 1
+    end
+    return true
+  end
+end
