2a7974c80bf4bd69ba669e9ff295610106833dc9
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 04:28:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,13 @@
 def prime?(num)
   num = num.abs
-  n = 1 
+  n = 4 
   count = 0 
   
-  
+  if (num%2 != 0 && num%3 != 0)
+    until n > num do
+      if (num%n == 0)
+        count += 1 
+        
 end
   
+  2 3 4 6 8 9 10 12 14 15 16 18
