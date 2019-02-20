ab8ef500e174246962b30cac1f96ad6ce67e5a9c
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 03:59:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   n = 1 
   count = 0 
   
-  until n >= Math.sqrt(num).floor do
+  until n > Math.sqrt(num).floor do
     if (num%n == 0 && count >= 3) || num == 1
       return false
     elsif (num%n == 0)
