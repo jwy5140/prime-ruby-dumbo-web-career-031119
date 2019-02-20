c56b94a663923d554ca40fb10567e3ae2e700ede
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 04:34:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,11 @@ def prime?(num)
   n = 4 
   count = 0 
   
-  if (num%2 != 0 && num%3 != 0)
-    until n > num do
-      if (num%n == 0)
-        count += 1 
+  if (num > 64)
+    if (num%2 != 0 && num%3 != 0)
+      while n < Math.sqrt(num).floor do
+        if (num%n == 0)
+          count += 1 
+          
         
 end
-  
-  2 3 4 6 8 9 10 12 14 15 16 18
