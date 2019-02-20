7298349d0b05754d0d686070e0602b65aac743e4
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 04:16:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,6 @@ def prime?(num)
   n = 1 
   count = 0 
   
-  while n < Math.sqrt(num).floor && num >= 2 do
-    if (num%n == 0)
-      count += 1 
-    end
-    if (n == Math.sqrt(num).floor-1 && count < 3)
-      return true 
-    end
-    n += 1 
-  end
-  return false
+  
 end
   
