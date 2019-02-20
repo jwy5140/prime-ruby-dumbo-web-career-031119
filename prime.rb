4193a178e4904dd7e1ba7407a41c035c3bbd2134
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 05:15:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,17 +7,15 @@ def prime?(num)
     while n < num do 
       if (num%n == 0)
         return false
-      else
-        return true
       end
     end
+    return true
   else
     while n < Math.sqrt(num).floor do
       if (num%n == 0)
         return false
-      else
-        return true
       end
     end
+    return true
   end
 end
