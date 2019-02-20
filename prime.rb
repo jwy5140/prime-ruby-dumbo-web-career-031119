428be5eb88156891ae6250b16c4b777a652c9ce9
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 04:08:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,17 @@ def prime?(num)
   n = 1 
   count = 0 
   
-  until n > Math.sqrt(num).floor do
-    if (num%n == 0 && count >= 3) || num < 2
-      return false
-    elsif (num%n == 0)
+  while n < Math.sqrt(num).floor && num > 4 do
+    if (num%n == 0)
       count += 1 
     end
-    n += 1
-    puts num
+    n += 1 
   end
-  return true
+  
+  if (count < 3)
+    return true
+  else
+    return false
+  end
 end
   
